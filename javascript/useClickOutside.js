0fe250723ab80ed032865011538fdc4370367a00
Andrew Iglinski <iglooworks1505@gmail.com>
2023-02-20 15:17:06 -0600

    Just started creating own library of useful React hooks so I don't have to rewrite these shits twice a week. Just create now, so only one hook added.

---
+++ javascript/useClickOutside.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react'
+
+export const useOnClickOutside = (ref, onClickOutside) => {
+    useEffect(
+        () => {
+            const listener = (event) => {
+                // Do nothing if clicking ref's element or descendent elements
+                if (!ref.current || ref.current.contains(event.target)) {
+                    return;
+                }
+                onClickOutside(event);
+            };
+            document.addEventListener("mousedown", listener);
+            document.addEventListener("touchstart", listener);
+            return () => {
+                document.removeEventListener("mousedown", listener);
+                document.removeEventListener("touchstart", listener);
+            };
+        },
+        [ref, onClickOutside]
+    );
+}
